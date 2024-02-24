2e5961527e6e0986253addffb5ac76d992fa1f66
jzpeterson <jordonpropm@gmail.com>
2024-02-24 11:44:52 -0700

    Make tsconfig changes to get tests working. I am not sure if this will work with DB migrations but...I will see.

--- upahead_lambda/rest-api/packages/functions/test/twilioWebhook.test.ts
+++ upahead_lambda/rest-api/packages/functions/test/twilioWebhook.test.ts
@@ -2,7 +2,7 @@ import {describe, expect, it, vi} from "vitest";
 import {handler} from "../src/twilioWebhook";
 import * as path from "path";
 import * as fs from "fs";
-import * as converssationManager from "@rest-api/core/conversations/conversationManager";
+import * as conversationManager from "@rest-api/core/conversations/conversationManager";
 
 
 vi.mock('@rest-api/core/conversations/conversationManager', () => ({
@@ -12,7 +12,7 @@ describe('Twilio handler function', () => {
     it('should return a 200 status code with a basic mock event', async () => {
         // Given
         const messageResponse = 'First Message';
-        converssationManager.handleIncomingMessage.mockResolvedValueOnce(messageResponse);
+        conversationManager.handleIncomingMessage.mockResolvedValueOnce(messageResponse);
 
         const mockEvent = {
             body: Buffer.from('Your message here').toString('base64'),
@@ -30,7 +30,7 @@ describe('Twilio handler function', () => {
     it('should return a 200 status code using a realistic twilio text message event', async () => {
         // Given
         const messageResponse = 'First Message';
-        converssationManager.handleIncomingMessage.mockResolvedValueOnce(messageResponse);
+        conversationManager.handleIncomingMessage.mockResolvedValueOnce(messageResponse);
 
         const jsonPath = path.join(__dirname, 'basic_text_message_webhook_event.json');
         const event = JSON.parse(fs.readFileSync(jsonPath, 'utf-8'));
