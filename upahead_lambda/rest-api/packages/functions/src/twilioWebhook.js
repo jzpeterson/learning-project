cb1f2b6e28e25f4d76a401ec1467940845286455
jzpeterson <jordonpropm@gmail.com>
2024-02-23 20:28:47 -0700

    Make migrations work

--- upahead_lambda/rest-api/packages/functions/src/twilioWebhook.js
+++ upahead_lambda/rest-api/packages/functions/src/twilioWebhook.js
@@ -1,8 +1,11 @@
-import { handle_incoming_mesage } from "@rest-api/core/conversations/conversationManager";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = void 0;
+const conversationManager_1 = require("@rest-api/core/conversations/conversationManager");
 const { MessagingResponse } = require("twilio").twiml;
-export const handler = async (event) => {
+const handler = async (event) => {
     console.log("Twilio Event Received: \n", event);
-    const nextMessage = await handle_incoming_mesage(event);
+    const nextMessage = await (0, conversationManager_1.handleIncomingMessage)(event);
     const twiml = new MessagingResponse();
     const message = twiml.message();
     message.body(nextMessage);
@@ -12,3 +15,4 @@ export const handler = async (event) => {
         body: twiml.toString(),
     };
 };
+exports.handler = handler;
--- upahead_lambda/rest-api/stacks/UpAheadStack.ts
+++ upahead_lambda/rest-api/stacks/UpAheadStack.ts
@@ -1,6 +1,9 @@
 import {Api, Config, RDS, StackContext} from "sst/constructs";
+import * as path from "path";
+
 
 export function UpAheadStack({stack}: StackContext) {
+    // const migrationsPath = path.join(__dirname, 'packages/core/src/db/migrations');
     const CONVERSATION_DB = "ConversationsDB";
 
     const TWILIO_ACCOUNT_SID = new Config.Secret(stack, "TWILIO_ACCOUNT_SID");
@@ -9,16 +12,11 @@ export function UpAheadStack({stack}: StackContext) {
     const cluster = new RDS(stack, "Cluster", {
         engine: "postgresql13.9",
         defaultDatabaseName: CONVERSATION_DB,
-        migrations: "packages/core/src/db/migrations",
-        types: {
-            path: "packages/core/src/db/types/types.ts",
-            camelCase: true
-        }
+        migrations: 'packages/core/src/db/migrations',
     });
 
     const api = new Api(stack, "Api", {
         defaults: {
-
             function: {
                 bind: [cluster, TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN],
             },
